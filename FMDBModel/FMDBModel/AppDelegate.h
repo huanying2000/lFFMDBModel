2910701a71309e0f746f60075c6b4a600d983fcd
ios开发 <[715416878@qq.com]>
2017-11-30 14:38:25 +0800

    初始化工程

---
+++ FMDBModel/FMDBModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FMDBModel
+//
+//  Created by ios开发 on 2017/11/30.
+//  Copyright © 2017年 ios开发. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
